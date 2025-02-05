0ce7a29677076558341c2f8301508dcd7a1208c7
Sonatype Zion <sonatype-zion@sonatype.com>
2025-02-05 20:12:29 +0000

    Release: 1.2.0

--- setup-iq-cli/dist/main/index.js
+++ setup-iq-cli/dist/main/index.js
@@ -31333,7 +31333,7 @@ module.exports = validRange
 "use strict";
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.IQ_CLI_DOWNLOAD_URL = exports.IQ_CLI_VERSION = exports.LATEST_IQ_VERSION = exports.MINIMUM_SUPPORTED_IQ_VERSION = exports.DOWNLOAD_URL = exports.IQ_VERSION_TO_COMPLETE = exports.IQ_CLI_JAR = void 0;
+exports.IQ_CLI_DOWNLOAD_URL = exports.IQ_CLI_VERSION = exports.LATEST_IQ_CLI_VERSION = exports.MINIMUM_SUPPORTED_IQ_VERSION = exports.DOWNLOAD_URL = exports.IQ_VERSION_TO_COMPLETE = exports.IQ_CLI_JAR = void 0;
 /*
  *  Copyright (c) 2023-present Sonatype, Inc. All rights reserved.
  *  Includes the third-party code listed at https://links.sonatype.com/products/clm/attributions.
@@ -31343,7 +31343,7 @@ exports.IQ_CLI_JAR = 'sonatype-iq-cli.jar';
 exports.IQ_VERSION_TO_COMPLETE = '1.{iq-cli-version}.0-01';
 exports.DOWNLOAD_URL = 'https://download.sonatype.com/clm/scanner/nexus-iq-cli-{iq-cli-version}.jar';
 exports.MINIMUM_SUPPORTED_IQ_VERSION = 137;
-exports.LATEST_IQ_VERSION = '2.0.0-01'; // This should be updated to the latest IQ CLI version with each release
+exports.LATEST_IQ_CLI_VERSION = '2.1.0-01'; // This should be updated to the latest IQ CLI version with each release
 exports.IQ_CLI_VERSION = 'iq-cli-version';
 exports.IQ_CLI_DOWNLOAD_URL = 'iq-cli-download-url';
 
@@ -31437,7 +31437,7 @@ const constants_1 = __nccwpck_require__(9733);
 function getValidatedIQCLIVersion() {
     const input = core.getInput(constants_1.IQ_CLI_VERSION);
     if (input === 'latest') {
-        return constants_1.LATEST_IQ_VERSION;
+        return constants_1.LATEST_IQ_CLI_VERSION;
     }
     if (/^\d+$/.test(input)) {
         if (parseInt(input) < constants_1.MINIMUM_SUPPORTED_IQ_VERSION) {
